20f7ef29a16f508f09d17000aae7a6a06a588a8d
enzo0989 <enzodeveloper01@gmail.com>
2023-10-13 19:31:39 -0300

    Added function to add tasks

--- project.py
+++ project.py
@@ -1,61 +1,72 @@
-import argparse, sys, csv
+import argparse, sys, csv, re
 
 def main():
     # Initialization of the program, it let's you pick either to load a previous list or to create a new one.
     parser = argparse.ArgumentParser(description="Creates a Todo list to achieve your goals.")
 
-    parser.add_argument("--load", metavar="load", type=str, help="Recreates an old list. The file must have been made using this program and have the CSV extension.")
     parser.add_argument("--create", metavar="create", type=str, help="Creates a new list. It must come with a name for the list. e.g tasks for the week.")
+    parser.add_argument("--load", metavar="load", type=str, 
+                        help="Loads a list. The file loaded must be inside the project directory and must have been made with this program.")
+
     args = parser.parse_args()
 
 
     if args.create:
-        print(load(args.create))
+        create()
     elif args.load:
-        print(load(args.load))
-    else:
-        print("not a valid action")
+        load(args.load)
 
-
-def create(name):
-    
-    return f"{name}"
-
-def load(filename):
-    # Non essential in this point of the project.
-    return f"{filename}"
-
-
+def create():
+        with open("TodoList.csv", "w") as file_list:
+            key_writer = csv.writer(file_list)
+            key_writer.writerow(["N°","task","date"])
+        action(file_list.name)
+        
+def load(filename): 
+    ...
 
 def action(list):
-    int = 0
+    i = 0
     while True:
         try:
-            action = input("action: ")
+            prompt = input("What kind of action do you want to perform?: ")
+            if prompt == "add":
+                i += 1
+                add(i,list)
+            elif prompt == "modify":
+                modify(list)
+            elif prompt == "delete":
+                delete(list)
+            elif prompt == "view":
+                view(list)
+            else:
+                pass
 
-            if action == "-add":
-                i += 1
-                add()
-            elif action == "-mod":
-                i += 1
-                modify()
-            elif action == "-del":
-                delete()
         except EOFError:
-            ...
-            # Ask if the user wants to save the list:
-            #   if the user writes Y save the file, 
-            #   else if the user writes N don't save it.
-            #   else prompt again.
-            # break the loop.
+            break
 
-def add(task, date):
-    ...
-def modify(number):
+
+def add(n,file):
+    global i
+    with open(file, "a") as todo_list:
+        task = input("What task do you want to add?: ")
+        date = input("by which day do you think you should finish this task?: ")
+        writer = csv.DictWriter(todo_list, fieldnames=["N°","task", "date"])
+        writer.writerow({"N°": n,"task": task,"date": date})
+
+    
+
+def modify(list):
     ...
 
-def delete(number):
+def delete(list):
     ...
+
+def view(list):
+    with open(list, "r") as file:
+        reader = csv.DictReader(file)
+
+        
 
 
 if __name__ == "__main__":
