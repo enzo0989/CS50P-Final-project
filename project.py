ebe0413b1c888614ea311055d012241f8577262b
enzo0989 <enzodeveloper01@gmail.com>
2023-10-14 18:50:02 -0300

    Added index
    To use it inside delete function

--- project.py
+++ project.py
@@ -22,18 +22,21 @@ def main():
 def create():
         with open("TodoList.csv", "w") as file_list:
             key_writer = csv.writer(file_list)
-            key_writer.writerow(["task","date"])
+            key_writer.writerow(["N","task","date"])
         action(file_list.name)
         
 def load(filename): 
     ...
 
 def action(list):
+    # i is used as and index.
+    i = 0
     while True:
         try:
             prompt = input("What kind of action do you want to perform?: ")
             if prompt == "add":
-                add(list)
+                add(i,list)
+                i += 1
             elif prompt == "modify":
                 modify(list)
             elif prompt == "delete":
@@ -46,13 +49,13 @@ def action(list):
             break
 
 
-def add(file):
+def add(n,file):
     with open(file, "a") as task_add:
         task = input("What task do you want to add?: ")
         date = input("by which day do you think you should finish this task?: ")
 
-        writer = csv.DictWriter(task_add, fieldnames=["task", "date"])
-        writer.writerow({"task": task,"date": date})
+        writer = csv.DictWriter(task_add, fieldnames=["N","task", "date"])
+        writer.writerow({"N":n,"task": task,"date": date})
 
     
 
@@ -82,8 +85,8 @@ def view(list):
     with open(list, "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            view_list.append({"task":row["task"],"date": row["date"]})
-    print(tabulate(view_list, tablefmt="heavy_grid" ,headers="keys", showindex="always"))
+            view_list.append({"N":row["N"],"task":row["task"],"date": row["date"]})
+    print(tabulate(view_list, tablefmt="heavy_grid" ,headers="keys"))
 
 
 if __name__ == "__main__":
